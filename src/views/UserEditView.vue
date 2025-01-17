e968305853bed2157dd80990cc7ffd30af07db75
Voke <wjw1469@gmail.com>
2025-01-17 10:30:19 +0900

    ✨ feat/test1

--- src/views/UserEditView.vue
+++ src/views/UserEditView.vue
@@ -1,5 +1,5 @@
 <script setup></script>
 <template>
-  <h1 class="text-7xl font-bold font-dnf text-white">User Edit</h1>
+  <h1 class="text-7xl font-bold font-dnf text-white">User 111Edit</h1>
 </template>
 <style scoped></style>

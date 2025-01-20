4866e4ed753cae0a0d62c26c2e278c286056feee
junyoung <jadw9715@gmail.com>
2025-01-21 00:36:19 +0900

    ✨ Feat: toast store

---
+++ src/components/common/Toast.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useToastStore } from "@/stores/toast";
+
+const { toasts, removeToast } = useToastStore();
+</script>
+<template>
+  <div class="fixed z-[999] bottom-7 right-7 flex flex-col gap-4">
+    <div class="w-[calc(100vw-32px)] max-w-[300px] p-4 rounded-lg bg-white">
+      성공
+    </div>
+  </div>
+</template>
+<style scoped></style>

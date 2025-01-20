afa6ac7b3fb4b422aa7bdbe7a320e8cbfacc020e
허정민 <gjwjdals96@naver.com>
2025-01-20 23:59:07 +0900

    :rotating_light: Fix: [게임디테일 랭크 컴포넌트] - [데이터 받을 수 있도록 코드 수정]

--- src/components/game-view/GameBanner.vue
+++ src/components/game-view/GameBanner.vue
@@ -10,21 +10,28 @@ export default {
   >
     <div class="absolute right-[56px] top-[126px]">
       <h1 class="text-6xl font-bold font-dnf text-white mb-10">Flappy Bird</h1>
-      <div class="rounded-[60px] w-[229px] h-[65px] bg-point-500 mt-[27px]">
-        <RouterLink
-          to="/game/play"
-          class="group rounded-[60px] w-[229px] h-[65px] bg-point-500 flex items-center justify-center gap-[12px]"
+      <RouterLink
+        to="/game/play"
+        class="group rounded-[60px] w-[229px] h-[65px] bg-point-500 hover:bg-white flex items-center justify-center gap-[12px] mt-[27px] transition-transform hover:scale-105"
+      >
+        <svg
+          width="18"
+          height="25"
+          viewBox="0 0 18 25"
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-[18px] h-[25px]"
         >
-          <img
-            src="/assets/images/icons/play-icon.svg"
-            alt="play icon"
-            class="w-[18px] h-[25px]"
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M5.44253 1.66346L16.8125 10.2615C17.5576 10.7916 18 11.6495 18 12.564C18 13.4784 17.5576 14.3363 16.8125 14.8665L5.44253 24.2655C4.48065 25.0351 3.17405 25.2149 2.04002 24.7338C0.905987 24.2526 0.127405 23.188 0.0125275 21.9615L0.0125275 3.96146C0.129372 2.73601 0.908712 1.67329 2.04238 1.19351C3.17605 0.713741 4.4815 0.894174 5.44253 1.66346Z"
+            class="fill-main-500 group-hover:fill-point-500"
           />
-          <p class="font-dnf text-white text-2xl group-hover:text-main-500">
-            Play Game
-          </p>
-        </RouterLink>
-      </div>
+        </svg>
+        <p class="font-dnf text-main-500 text-2xl group-hover:text-point-500">
+          Play Game
+        </p>
+      </RouterLink>
     </div>
   </div>
 </template>
--- src/components/game-view/GameRank.vue
+++ src/components/game-view/GameRank.vue
@@ -1,6 +1,132 @@
-<script>
-export default {
-  name: "GameRank",
+<script setup>
+import { ref } from "vue";
+import rank1st from "/assets/images/rank_1st.png";
+import rank2nd from "/assets/images/rank_2nd.png";
+import rank3rd from "/assets/images/rank_3rd.png";
+
+const rankData = ref([
+  {
+    id: "1",
+    rank: 1,
+    user_id: "싹싹이",
+    game_id: "tetris",
+    score: 9999,
+    play_time: 300,
+    total_play_time: 3600,
+    create_at: "2024-01-20T12:00:00",
+    updated_at: null,
+  },
+  {
+    id: "2",
+    rank: 2,
+    user_id: "이준영",
+    game_id: "tetris",
+    score: 9900,
+    play_time: 280,
+    total_play_time: 3200,
+    create_at: "2024-01-20T11:00:00",
+    updated_at: null,
+  },
+  {
+    id: "3",
+    rank: 3,
+    user_id: "e1a4",
+    game_id: "tetris",
+    score: 9900,
+    play_time: 280,
+    total_play_time: 3200,
+    create_at: "2024-01-20T11:00:00",
+    updated_at: null,
+  },
+  {
+    id: "4",
+    rank: 4,
+    user_id: "gjwjdals96",
+    game_id: "tetris",
+    score: 8123,
+    play_time: 200,
+    total_play_time: 2400,
+    create_at: "2024-01-20T10:00:00",
+    updated_at: null,
+  },
+  {
+    id: "5",
+    rank: 5,
+    user_id: "싹싹김치",
+    game_id: "tetris",
+    score: 7133,
+    play_time: 200,
+    total_play_time: 2400,
+    create_at: "2024-01-20T10:00:00",
+    updated_at: null,
+  },
+  {
+    id: "6",
+    rank: 6,
+    user_id: "감자",
+    game_id: "tetris",
+    score: 6234,
+    play_time: 200,
+    total_play_time: 2400,
+    create_at: "2024-01-20T10:00:00",
+    updated_at: null,
+  },
+  {
+    id: "7",
+    rank: 7,
+    user_id: "comma",
+    game_id: "tetris",
+    score: 5223,
+    play_time: 200,
+    total_play_time: 2400,
+    create_at: "2024-01-20T10:00:00",
+    updated_at: null,
+  },
+  {
+    id: "8",
+    rank: 8,
+    user_id: "저녁메뉴추천좀",
+    game_id: "tetris",
+    score: 4897,
+    play_time: 200,
+    total_play_time: 2400,
+    create_at: "2024-01-20T10:00:00",
+    updated_at: null,
+  },
+  {
+    id: "9",
+    rank: 9,
+    user_id: "랄랄라라라라라",
+    game_id: "tetris",
+    score: 1212,
+    play_time: 200,
+    total_play_time: 2400,
+    create_at: "2024-01-20T10:00:00",
+    updated_at: null,
+  },
+  {
+    id: "10",
+    rank: 10,
+    user_id: "랄랄라",
+    game_id: "tetris",
+    score: 1089,
+    play_time: 200,
+    total_play_time: 2400,
+    create_at: "2024-01-20T10:00:00",
+    updated_at: null,
+  },
+]);
+const getRankImage = (rank) => {
+  switch (rank) {
+    case 1:
+      return rank1st;
+    case 2:
+      return rank2nd;
+    case 3:
+      return rank3rd;
+    default:
+      return null;
+  }
 };
 </script>
 
@@ -13,110 +139,52 @@ export default {
       RANKING
     </h2>
     <div class="mt-[20px] flex flex-col gap-[12px]">
-      <div
-        class="w-[272px] h-[65px] bg-point-500 rounded-[12px] pl-[20px] pr-[19px] py-[13px] flex item-center justify-between"
+      <RouterLink
+        v-for="item in rankData"
+        :key="item.id"
+        :to="`/user/${item.user_id}`"
+        class="block"
       >
-        <img
-          src="/assets/images/rank_1st.png?url"
-          alt="1st"
-          class="w-[42px] h-[39px]"
-        />
-        <span
-          class="font-dnf text-base text-white flex items-center max-w-[100px] truncate"
-          >싹싹이
-        </span>
-        <span class="text-sm text-white flex items-center">9999점</span>
-      </div>
-      <div
-        class="w-[272px] h-[65px] bg-point-600 rounded-[12px] pl-[20px] pr-[19px] py-[13px] flex item-center justify-between"
-      >
-        <img
-          src="/assets/images/rank_2nd.png?url"
-          alt="2nd"
-          class="w-[42px] h-[39px]"
-        />
-        <span
-          class="font-dnf text-base text-white flex items-center max-w-[100px] truncate"
-          >이준영</span
+        <div
+          class="w-[272px] h-[65px] rounded-[12px] pl-[20px] pr-[19px] py-[13px] flex items-center justify-between"
+          :class="{
+            'bg-point-500': item.rank === 1,
+            'bg-point-600': item.rank === 2,
+            'bg-point-700': item.rank === 3,
+            'bg-main-500': item.rank > 3,
+          }"
         >
-        <span class="text-sm text-white flex items-center">9900점</span>
-      </div>
-      <div
-        class="w-[272px] h-[65px] bg-point-700 rounded-[12px] pl-[20px] pr-[19px] py-[13px] flex item-center justify-between"
-      >
-        <img
-          src="/assets/images/rank_3rd.png?url"
-          alt="3rd"
-          class="w-[42px] h-[39px]"
-        />
-        <span class="font-dnf text-base text-white flex items-center"
-          >랄랄라</span
-        >
-        <span class="text-sm text-white flex items-center">9000점</span>
-      </div>
-      <div
-        class="w-[272px] h-[65px] bg-main-500 rounded-[12px] pl-[32px] pr-[19px] py-[13px] flex item-center justify-between"
-      >
-        <span class="font-dnf text-xl text-white flex items-center">4</span>
-        <span class="font-dnf text-base text-white flex items-center ml-[15px]">
-          랄랄라</span
-        >
-        <span class="text-sm text-white flex items-center">9000점</span>
-      </div>
-      <div
-        class="w-[272px] h-[65px] bg-main-500 rounded-[12px] pl-[32px] pr-[19px] py-[13px] flex item-center justify-between"
-      >
-        <span class="font-dnf text-xl text-white flex items-center">5</span>
-        <span class="font-dnf text-base text-white flex items-center ml-[15px]">
-          랄랄라</span
-        >
-        <span class="text-sm text-white flex items-center">9000점</span>
-      </div>
-      <div
-        class="w-[272px] h-[65px] bg-main-500 rounded-[12px] pl-[32px] pr-[19px] py-[13px] flex item-center justify-between"
-      >
-        <span class="font-dnf text-xl text-white flex items-center">6</span>
-        <span class="font-dnf text-base text-white flex items-center ml-[15px]">
-          랄랄라</span
-        >
-        <span class="text-sm text-white flex items-center">9000점</span>
-      </div>
-      <div
-        class="w-[272px] h-[65px] bg-main-500 rounded-[12px] pl-[32px] pr-[19px] py-[13px] flex item-center justify-between"
-      >
-        <span class="font-dnf text-xl text-white flex items-center">7</span>
-        <span class="font-dnf text-base text-white flex items-center ml-[15px]">
-          랄랄라라라라라라</span
-        >
-        <span class="text-sm text-white flex items-center">9000점</span>
-      </div>
-      <div
-        class="w-[272px] h-[65px] bg-main-500 rounded-[12px] pl-[32px] pr-[19px] py-[13px] flex item-center justify-between"
-      >
-        <span class="font-dnf text-xl text-white flex items-center">8</span>
-        <span class="font-dnf text-base text-white flex items-center ml-[15px]">
-          랄랄라</span
-        >
-        <span class="text-sm text-white flex items-center">9000점</span>
-      </div>
-      <div
-        class="w-[272px] h-[65px] bg-main-500 rounded-[12px] pl-[32px] pr-[19px] py-[13px] flex item-center justify-between"
-      >
-        <span class="font-dnf text-xl text-white flex items-center">9</span>
-        <span class="font-dnf text-base text-white flex items-center ml-[15px]">
-          랄랄라</span
-        >
-        <span class="text-sm text-white flex items-center">9000점</span>
-      </div>
-      <div
-        class="w-[272px] h-[65px] bg-main-500 rounded-[12px] pl-[28px] pr-[19px] py-[13px] flex item-center justify-between"
-      >
-        <span class="font-dnf text-xl text-white flex items-center">10</span>
-        <span class="font-dnf text-base text-white flex items-center ml-[8px]">
-          랄랄라</span
-        >
-        <span class="text-sm text-white flex items-center">9000점</span>
-      </div>
+          <template v-if="item.rank <= 3">
+            <img
+              :src="getRankImage(item.rank)"
+              :alt="`${item.rank}st`"
+              class="w-[42px] h-[39px]"
+            />
+          </template>
+          <template v-else>
+            <span
+              class="font-dnf text-xl text-white flex items-center"
+              :class="{
+                'ml-[13px]': item.rank > 3,
+                'ml-[4.8px]': item.rank === 10,
+              }"
+              >{{ item.rank }}</span
+            >
+          </template>
+          <span
+            class="font-dnf text-base text-white block max-w-[80px] truncate overflow-hidden uppercase"
+            :class="{
+              'ml-[13px]': item.rank > 3,
+              'ml-[6.8px]': item.rank === 10,
+            }"
+          >
+            {{ item.user_id }}
+          </span>
+          <span class="text-sm text-white flex items-center"
+            >{{ item.score }}점</span
+          >
+        </div>
+      </RouterLink>
     </div>
   </div>
 </template>

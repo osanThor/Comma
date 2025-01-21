2eb55bf4970e95b6555e902f03ccf1d4b4007d6e
허정민 <gjwjdals96@naver.com>
2025-01-21 23:48:12 +0900

    :sparkles: Feature:게임배너, 게임리스트, 게임설명, 게임랭킹 url별 렌더링 및 api 연결 완료

--- src/components/game-view/GameIntro.vue
+++ src/components/game-view/GameIntro.vue
@@ -1,18 +1,16 @@
-<script>
-import GameDescription from "../game-view/GameDescription.vue";
-export default {
-  name: "GameIntro",
-  components: {
-    GameDescription,
+<script setup>
+defineProps({
+  intro: {
+    type: String,
+    required: true,
   },
-};
+});
 </script>
 
 <template>
   <div
-    class="h-[108px] w-[287px] bg-main-600 text-sm text-white text-center pt-[27px] pr-[18px] pb-[19px] pl-[19px] items-center justify-center mb-[38px]"
+    class="h-[108px] w-[287px] bg-main-600 text-sm text-white text-center pt-[27px] pr-[18px] pb-[19px] pl-[19px] items-center justify-center mb-[38px] whitespace-pre-line"
   >
-    플래피 버드는 단순하지만 중독성 있는 게임으로, 스페이스바를 통해 새를
-    조작하여 장애물을 피하는 방식으로 진행됩니다.
+    {{ intro }}
   </div>
 </template>

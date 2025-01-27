ff780c249966196a2549741277c83560e942d446
choiboa <musamea99@gmail.com>
2025-01-27 12:17:23 +0900

    🚨 Fix: Flappy - 캔버스 사이즈 버그 수정 및 게임오버 모달 추가

--- src/components/game/FlappyBird.vue
+++ src/components/game/FlappyBird.vue
@@ -1,47 +1,68 @@
 <template>
   <div class="game-scene">
     <!-- 게임화면 렌더링 -->
-    <canvas ref="gameCanvas" width="1300" height="750"></canvas>
+    <canvas ref="gameCanvas" width="1300" height="750" style="width: 100%; height: 100%;"></canvas>
   </div>
 </template>
 
-<script>
+<script setup >
+import { ref, onMounted, onBeforeUnmount } from 'vue';
 import kaplay from "kaplay";
 import { useGameStore } from "../../stores/gameStore.js";
 import { SCALE_FACTOR } from "../../constants/flappy.js";
 import { makeBackground } from "../../classes/flappy/makeBackground.js";
 import { makePlayer } from "../../classes/flappy/makePlayer.js";
-import { makeScoreBox } from "../../classes/flappy/makeScoreBox.js";
 import { saveSystem } from "../../classes/flappy/save.js";
 import { useTimer } from "@/hooks/useTimer.js";
 
-export default {
-  name: "GameScene",
-  async mounted() {
-    const { currentTime, start, stop, reset } = useTimer();
+const emit = defineEmits(["open-game-over"]);
 
-    const canvas = this.$refs.gameCanvas;
-    const k = kaplay({
+const gameCanvas = ref(null);
+
+const setCanvasSize = () => {
+  const canvas = gameCanvas.value;
+  if (canvas) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
+};
+
+let audioEnabled = ref(true);
+const gameStore = useGameStore();
+let isGameStarted = ref(false);
+let audioContext = null;
+let k = null;
+
+const { currentTime, start, stop, reset } = useTimer();
+
+const resumeAudioContext = () => {
+  if (!audioContext) {
+    audioContext = new AudioContext();
+  }
+  audioContext.resume().then(() => {
+    console.log("Audio context resumed");
+  });
+};
+
+const startGame =() => {
+  isGameStarted.value = true;
+  k.setGravity(2500);
+  start();
+}
+
+onMounted(async () => {
+  setCanvasSize(); 
+  window.addEventListener('resize', setCanvasSize);
+
+    const canvas = gameCanvas.value;
+    k = kaplay({
       width: 1300,
       height: 750,
       letterbox: true,
       global: true,
       canvas: canvas,
     });
-
-    let audioEnabled = true;
-    const gameStore = useGameStore();
-    let isGameStarted = false;
-    let audioContext;
-
-    const resumeAudioContext = () => {
-      if (!audioContext) {
-        audioContext = new AudioContext();
-      }
-      audioContext.resume().then(() => {
-        console.log("Audio context resumed");
-      });
-    };
+    
 
     k.loadSprite("sky", "/assets/flappy/2.png");
     k.loadSprite("field", "/assets/flappy/3.png");
@@ -94,11 +115,6 @@ export default {
       k.onKeyPress("space", goToGame);
     });
 
-    function startGame() {
-      isGameStarted = true;
-      k.setGravity(2500);
-      start();
-    }
 
     // 메인 게임 씬
     k.scene("main", async () => {
@@ -106,7 +122,7 @@ export default {
       let score = 0;
       let playTime = 0;
 
-      isGameStarted = false;
+      isGameStarted.value = false;
 
       makeBackground(k);
 
@@ -114,7 +130,7 @@ export default {
 
       const clouds = map.add([k.sprite("clouds"), k.pos(), { speed: 5 }]);
       clouds.onUpdate(() => {
-        if (isGameStarted) {
+        if (isGameStarted.value) {
           clouds.move(clouds.speed, 0);
           if (clouds.pos.x > 500) {
             clouds.pos.x = -100;
@@ -147,7 +163,7 @@ export default {
       };
 
       k.onUpdate(() => {
-        if (!isGameStarted) return;
+        if (!isGameStarted.value) return;
 
         for (let i = 0; i < obstaclesLayer.parts.length; i++) {
           const currentPart = obstaclesLayer.parts[i];
@@ -160,7 +176,7 @@ export default {
 
           currentPart.move(obstaclesLayer.speed, 0);
         }
-        if (isGameStarted) {
+        if (isGameStarted.value) {
           obstaclesLayer.speed -= 0.1;
         }
       });
@@ -186,11 +202,11 @@ export default {
       k.add([k.rect(k.width(), 50), k.pos(0, 1000), k.area(), "obstacle"]);
 
       k.onKeyPress("space", () => {
-        if (!isGameStarted) {
+        if (!isGameStarted.value) {
           startGame();
         } else if (!player.isDead) {
           player.jump(400);
-          if (audioEnabled) k.play("jump", { volume: 0.02 });
+          if (audioEnabled.value) k.play("jump", { volume: 0.02 });
         }
       });
 
@@ -201,14 +217,14 @@ export default {
       // 충돌한 후 게임 점수/타임 기록
       player.onCollide("obstacle", async () => {
         if (player.isDead) return;
-        if (audioEnabled) k.play("hurt");
+        if (audioEnabled.value) k.play("hurt");
 
-        isGameStarted = false;
+        isGameStarted.value = false;
 
         obstaclesLayer.speed = 0;
         map.speed = 0;
         player.disableControls();
-        makeScoreBox(k, k.center(), score, currentTime);
+        emit("open-game-over", score, currentTime);
         player.isDead = true;
 
         // 최고 점수 갱신 및 저장
@@ -219,11 +235,9 @@ export default {
 
         // 점수 저장
         try {
-          // gameStore.updateMaxScore(score);
           await saveSystem.load();
           if (score > saveSystem.data.maxScore) {
             saveSystem.data.maxScore = score;
-            // await saveSystem.save();
           }
           saveSystem.data.playTime = playTime;
           await saveSystem.save();
@@ -236,7 +250,7 @@ export default {
 
       k.setCamScale(k.vec2(1.2));
       player.onUpdate(() => {
-        if (isGameStarted && !player.isDead) {
+        if (isGameStarted.value && !player.isDead) {
           k.setCamPos(player.pos.x + 100, 400);
         }
       });
@@ -248,6 +262,9 @@ export default {
 
     // 게임 시작
     k.go("start");
-  },
-};
+  });
+
+  onBeforeUnmount(() => {
+  window.removeEventListener('resize', setCanvasSize); 
+});
 </script>

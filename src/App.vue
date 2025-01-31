e806db5d33a54b66f09fc4bfcd0d1b51a6fa16fc
junyoung <jadw9715@gmail.com>
2025-01-31 21:00:19 +0900

    🔨 Refactor. app vue 경로

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import EmptyLayout from "@/layouts/EmptyLayout.vue";
-import Toast from "./components/common/Toast.vue";
-import Modal from "./components/common/Modal.vue";
+import Toast from "@/components/common/Toast.vue";
+import Modal from "@/components/common/Modal.vue";
 const route = useRoute();
 const layout = computed(() => {
   const layout = route.meta.layout || "DefaultLayout";
